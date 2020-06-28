52bf4951c209184c1b149ad243b9a9337013c92b
Akshat Pandey <akshat.pandey383@gmail.com>
2020-06-28 22:06:27 +0530

    Updates

---
+++ precache-manifest.39dfe885805e9849bc998c27d48d7b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86826561ca79d6ae924f5787cc72215",
+    "url": "/aksh29/businessapp/index.html"
+  },
+  {
+    "revision": "867c005ae581df5c4b00",
+    "url": "/aksh29/businessapp/static/css/main.2f912210.chunk.css"
+  },
+  {
+    "revision": "3182e58754227ee16749",
+    "url": "/aksh29/businessapp/static/js/2.1e5d13bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aksh29/businessapp/static/js/2.1e5d13bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867c005ae581df5c4b00",
+    "url": "/aksh29/businessapp/static/js/main.bacdff6c.chunk.js"
+  },
+  {
+    "revision": "c1e766c9f3f965c9bd42",
+    "url": "/aksh29/businessapp/static/js/runtime-main.4c24cc31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aksh29/businessapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
